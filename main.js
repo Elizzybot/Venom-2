28da7eac3ba0a18c3eb7635ff71858f480fd9746
Elizzybot <elijahchinecheremonah@gmail.com>
2024-08-08 19:37:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const store = makeInMemoryStore({
     })
 })
 
-let phoneNumber = "2349066528353"
+let phoneNumber = "2347038494997"
 let owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
